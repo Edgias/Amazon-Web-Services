37049d71dd54c68ed6f123a60e124be0941413af
Edgias Mudzukwa <mudzukwae@outlook.com>
2020-12-08 09:40:31 +0200

    Added credentials to access bucket.

---
+++ Edgias.AmazonS3/BucketManager.cs
@@ -0,0 +1,68 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Edgias.Study.AWS
+{
+    public class BucketManager
+    {
+        //private const string _bucketName = "edgiasfirstbucket";
+
+        private static IAmazonS3 _client;
+
+        public BucketManager(AmazonS3Client client)
+        {
+            _client = client;
+        }
+
+        public async Task<IEnumerable<S3Object>> GetObjectsAsync(string bucketName)
+        {
+            try
+            {
+                ListObjectsRequest request = new ListObjectsRequest
+                {
+                    BucketName = bucketName,
+                    MaxKeys = 10
+                };
+
+                List<S3Object> bucketObjects = new List<S3Object>();
+
+                do
+                {
+                    ListObjectsResponse response = await _client.ListObjectsAsync(request);
+
+                    // Process the response.
+                    foreach (S3Object entry in response.S3Objects)
+                    {
+                        bucketObjects.Add(entry);
+                    }
+
+                    // If the response is truncated, set the marker to get the next 
+                    // set of keys.
+                    if (response.IsTruncated)
+                    {
+                        request.Marker = response.NextMarker;
+                    }
+                    else
+                    {
+                        request = null;
+                    }
+                } while (request != null);
+
+                return bucketObjects;
+            }
+
+            catch (AmazonS3Exception)
+            {
+                throw;
+            }
+
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
--- Edgias.AmazonS3/Program.cs
+++ Edgias.AmazonS3/Program.cs
@@ -1,28 +1,88 @@
 ﻿using Amazon;
+using Amazon.Runtime.CredentialManagement;
 using Amazon.S3;
+using Amazon.S3.Model;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Edgias.Study.AWS.AmazonS3
 {
     class Program
     {
-        private static readonly RegionEndpoint _bucketRegion = RegionEndpoint.USEast2;
-
         static async Task Main(string[] args)
         {
-            MakeS3Request makeS3Request;
-            using (var client = new AmazonS3Client(_bucketRegion))
+            try
             {
-                makeS3Request = new MakeS3Request(client);
+                RegionEndpoint _bucketRegion = RegionEndpoint.USEast2;
+                string _bucketName = "edgiasfirstbucket";
 
-                Console.WriteLine("Listing objects stored in a bucket");
+                CredentialProfileStoreChain chain = new CredentialProfileStoreChain();
+                bool profileFound = chain.TryGetProfile("basic_profile", out CredentialProfile basicProfile);
 
-                await makeS3Request.ListingObjectsAsync();
+                // If profile doesn't exist try to create it.
+                if (!profileFound)
+                {
+                    RegisterProfile(_bucketRegion);
+                    bool profileFoundAfterRegistration = chain.TryGetProfile("basic_profile", out basicProfile);
+
+                    if (!profileFoundAfterRegistration)
+                    {
+                        Console.WriteLine("Profile was not found");
+                        return;
+                    }
+                }
+
+                using (var client = new AmazonS3Client(basicProfile.Options.AccessKey, basicProfile.Options.SecretKey, _bucketRegion))
+                {
+                    BucketManager bucketManager = new BucketManager(client);
+
+                    Console.WriteLine("Listing objects stored in a bucket, please wait...");
+
+                    IEnumerable<S3Object> objects = await bucketManager.GetObjectsAsync(_bucketName);
+
+                    foreach (S3Object entry in objects)
+                    {
+                        Console.WriteLine("key = {0} size = {1}", entry.Key, entry.Size);
+                    }
+                }
+
+                Console.WriteLine("Done");
+
+                Console.Read();
             }
 
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
 
-            Console.Read();
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Register a profile to access a bucket using your IAM credentials
+        /// </summary>
+        /// <param name="regionEndpoint">Region to use</param>
+        static void RegisterProfile(RegionEndpoint regionEndpoint)
+        {
+            CredentialProfileOptions options = new CredentialProfileOptions
+            {
+                AccessKey = "access_key",
+                SecretKey = "secret_key"
+            };
+
+            CredentialProfile profile = new CredentialProfile("basic_profile", options)
+            {
+                Region = regionEndpoint
+            };
+
+            NetSDKCredentialsFile netSDKFile = new NetSDKCredentialsFile();
+
+            netSDKFile.RegisterProfile(profile);
         }
     }
 }
